01a795e6448bd8af59452e8a5f1086d62d4af72f
Rodnext <[leoinfotecrj@gmail.com]>
2024-06-09 16:30:26 -0300

    1.0.3

--- js/script.js
+++ js/script.js
@@ -1,30 +1,6 @@
-/*function scrollToElement(elementSelector, instance = 0) {
-  const elements = document.querySelectorAll(elementSelector);
-  if (elements.length > instance) {
-    elements[instance].scrollIntoView({ behavior: "smooth" });
-  }
-}
-
-const link1 = document.getElementById("link1");
-const link2 = document.getElementById("link2");
-const link3 = document.getElementById("link3");
-
-link1.addEventListen('click', () =>{
-    scrollToElement('.header');
-});
-link2.addEventListen('click', () =>{
-    scrollToElement('.header', 1);
-});
-link3.addEventListen('click', () =>{
-    scrollToElement('.coluna');
-});
-*/
 function scrollToElement(elementSelector, instance = 0) {
-    // Select all elements that match the given selector
     const elements = document.querySelectorAll(elementSelector);
-    // Check if there are elements matching the selector and if the requested instance exists
     if (elements.length > instance) {
-        // Scroll to the specified instance of the element
         elements[instance].scrollIntoView({ behavior: 'smooth' });
     }
 }
@@ -38,7 +14,6 @@ link1.addEventListener('click', () => {
 });
 
 link2.addEventListener('click', () => {
-    // Scroll to the second element with "header" class
     scrollToElement('.header', 1);
 });
 
